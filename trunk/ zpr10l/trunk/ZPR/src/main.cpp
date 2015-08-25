dcbfa8bf2a3d7aefe422504fdf933742f4e4c84a
Your Name <you@example.com>
2015-08-25 21:30:10 +0600

    init

---
+++ trunk/ zpr10l/trunk/ZPR/src/main.cpp
@@ -0,0 +1,20 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 2010-03-10
+ *      Author: Administrator
+ */
+
+#include <boost/lambda/lambda.hpp>
+#include <iostream>
+#include <iterator>
+#include <algorithm>
+
+int main()
+{
+    using namespace boost::lambda;
+    typedef std::istream_iterator<int> in;
+
+    std::for_each(
+        in(std::cin), in(), std::cout << (_1 * 3) << " " );
+}
